80f9ad5d72855010b1f3895c70e7f9ec442043f5
FullLifeGames <fulllifegames@gmail.com>
2020-06-22 20:15:59 +0200

    Make everything prettier

--- src/injection.ts
+++ src/injection.ts
@@ -1,93 +1,104 @@
 import {
-	translateEvs,
-	getSearchParameters,
-	handleInput,
-	getSetPokemon,
-	isTierAvailable,
-	describeCalcs,
-	handleOutput,
-} from "./util.js";
+  translateEvs,
+  getSearchParameters,
+  handleInput,
+  getSetPokemon,
+  isTierAvailable,
+  describeCalcs,
+  handleOutput,
+} from './util.js';
 
-declare var SETDEX: any;
+declare let SETDEX: any;
 declare class Generation {
-	constructor(generation: number);
-};
+  constructor(generation: number);
+}
 declare class Pokemon {
-	constructor(generation: Generation, pokemon: string, setOwn: any);
-	evs: any;
-};
+  constructor(generation: Generation, pokemon: string, setOwn: any);
+  evs: any;
+}
 
 $(function () {
+  const getParameters = getSearchParameters();
 
-	var getParameters = getSearchParameters();
+  let ownPokemon = getParameters['ownPokemon'];
+  ownPokemon = handleInput(ownPokemon);
+  let oppPokemon = getParameters['oppPokemon'];
+  oppPokemon = handleInput(oppPokemon);
 
-	var ownPokemon = getParameters['ownPokemon'];
-	ownPokemon = handleInput(ownPokemon);
-	var oppPokemon = getParameters['oppPokemon'];
-	oppPokemon = handleInput(oppPokemon);
+  const generation = parseInt(getParameters['tier'][3]);
+  const mode = getParameters['tier'].slice(4);
+  const setdex = SETDEX[generation];
 
-	var generation = parseInt(getParameters['tier'][3]);
-	var mode = getParameters['tier'].slice(4);
-	var setdex = SETDEX[generation];
+  ownPokemon = getSetPokemon(setdex, ownPokemon);
+  const setsOwn = setdex[ownPokemon];
+  oppPokemon = getSetPokemon(setdex, oppPokemon);
+  const setsOpp = setdex[oppPokemon];
+  const owncalcs = {} as any;
+  const oppcalcs = {} as any;
 
-	ownPokemon = getSetPokemon(setdex, ownPokemon);
-	var setsOwn = setdex[ownPokemon];
-	oppPokemon = getSetPokemon(setdex, oppPokemon);
-	var setsOpp = setdex[oppPokemon];
-	var owncalcs = {} as any;
-	var oppcalcs = {} as any;
+  const gen = new Generation(generation);
 
-	var gen = new Generation(generation);
+  const hasOwnCustomSet = Object.values(setsOwn).some(
+    (entry: any) => 'isCustomSet' in entry
+  );
+  const hasOppCustomSet = Object.values(setsOpp).some(
+    (entry: any) => 'isCustomSet' in entry
+  );
 
-	var hasOwnCustomSet = Object.values(setsOwn).some((entry : any) => 'isCustomSet' in entry);
-	var hasOppCustomSet = Object.values(setsOpp).some((entry : any) => 'isCustomSet' in entry);
+  const ownTierAvailable = isTierAvailable(setsOwn, mode);
 
-	var ownTierAvailable = isTierAvailable(setsOwn, mode);
+  for (const key in setsOwn) {
+    if (hasOwnCustomSet && !('isCustomSet' in setsOwn[key])) {
+      continue;
+    }
+    if (
+      ownTierAvailable &&
+      key.toLowerCase().indexOf(mode + ' ') === -1 &&
+      !hasOwnCustomSet
+    ) {
+      continue;
+    }
+    owncalcs[key] = {};
+    const setOwn = setsOwn[key];
+    translateEvs(setOwn);
 
-	for (var key in setsOwn) {
-		if (hasOwnCustomSet && !('isCustomSet' in setsOwn[key])) {
-			continue;
-		}
-		if (ownTierAvailable && key.toLowerCase().indexOf(mode + " ") === -1 && !hasOwnCustomSet) {
-			continue;
-		}
-		owncalcs[key] = {};
-		var setOwn = setsOwn[key];
-		translateEvs(setOwn);
+    const oppTierAvailable = isTierAvailable(setsOpp, mode);
 
-		var oppTierAvailable = isTierAvailable(setsOpp, mode);
+    for (const key2 in setsOpp) {
+      if (hasOppCustomSet && !('isCustomSet' in setsOpp[key2])) {
+        continue;
+      }
+      if (
+        oppTierAvailable &&
+        key2.toLowerCase().indexOf(mode + ' ') === -1 &&
+        !hasOppCustomSet
+      ) {
+        continue;
+      }
+      const setOpp = setsOpp[key2];
+      if (!(key2 in oppcalcs)) {
+        oppcalcs[key2] = {};
+      }
+      if (!(key2 in owncalcs[key])) {
+        owncalcs[key][key2] = [];
+      }
+      if (!(key in oppcalcs[key2])) {
+        oppcalcs[key2][key] = [];
+      }
+      translateEvs(setOpp);
+      const pokeOwn = new Pokemon(gen, ownPokemon, setOwn);
+      if ('evs' in setOwn) pokeOwn.evs = setOwn.evs;
+      const pokeOpp = new Pokemon(gen, oppPokemon, setOpp);
+      if ('evs' in setOpp) pokeOpp.evs = setOpp.evs;
+      describeCalcs(pokeOwn, gen, pokeOpp, generation, owncalcs, key, key2);
+      describeCalcs(pokeOpp, gen, pokeOwn, generation, oppcalcs, key2, key);
+    }
+  }
 
-		for (var key2 in setsOpp) {
-			if (hasOppCustomSet && !('isCustomSet' in setsOpp[key2])) {
-				continue;
-			}
-			if (oppTierAvailable && key2.toLowerCase().indexOf(mode + " ") === -1 && !hasOppCustomSet) {
-				continue;
-			}
-			var setOpp = setsOpp[key2];
-			if (!(key2 in oppcalcs)) {
-				oppcalcs[key2] = {};
-			}
-			if (!(key2 in owncalcs[key])) {
-				owncalcs[key][key2] = [];
-			}
-			if (!(key in oppcalcs[key2])) {
-				oppcalcs[key2][key] = [];
-			}
-			translateEvs(setOpp);
-			var pokeOwn = new Pokemon(gen, ownPokemon, setOwn);
-			if ("evs" in setOwn) pokeOwn.evs = setOwn.evs;
-			var pokeOpp = new Pokemon(gen, oppPokemon, setOpp);
-			if ("evs" in setOpp) pokeOpp.evs = setOpp.evs;
-			describeCalcs(pokeOwn, gen, pokeOpp, generation, owncalcs, key, key2);
-			describeCalcs(pokeOpp, gen, pokeOwn, generation, oppcalcs, key2, key);
-		}
-	}
+  let output = '';
 
-	var output = '';
+  output += handleOutput(ownPokemon, oppPokemon, owncalcs, oppcalcs);
+  output += handleOutput(oppPokemon, ownPokemon, oppcalcs, owncalcs);
 
-	output += handleOutput(ownPokemon, oppPokemon, owncalcs, oppcalcs);
-	output += handleOutput(oppPokemon, ownPokemon, oppcalcs, owncalcs);
-
-	$('#results').html(output);
+  $('#results').html(output);
 });
--- src/util.ts
+++ src/util.ts
@@ -1,184 +1,208 @@
 // Pure private utility
 
-const getDifference = (a: string, b: string) : string => {
-	var i = 0;
-	var j = 0;
-	var result = "";
+const getDifference = (a: string, b: string): string => {
+  let i = 0;
+  let j = 0;
+  let result = '';
 
-	while (j < b.length) {
-		if (a[i] != b[j] || i == a.length)
-			result += b[j];
-		else
-			i++;
-		j++;
-	}
-	return result;
+  while (j < b.length) {
+    if (a[i] != b[j] || i == a.length) result += b[j];
+    else i++;
+    j++;
+  }
+  return result;
 };
 
 const argmin = (arr: number[]) => {
-	if (!Array.isArray(arr)) {
-		throw new TypeError('argmin()::invalid input argument. Must provide an array.');
-	}
-	var len = arr.length,
-		min = arr[0],
-		idx = [0],
-		val;
+  if (!Array.isArray(arr)) {
+    throw new TypeError(
+      'argmin()::invalid input argument. Must provide an array.'
+    );
+  }
+  const len = arr.length;
+  let min = arr[0];
+  const idx = [0];
+  let val;
 
-	for (var i = 1; i < len; i++) {
-		val = arr[i];
-		if (val < min) {
-			min = val;
-			idx.length = 0;
-			idx.push(i);
-		} else if (val === min) {
-			idx.push(i);
-		}
-	}
-	return idx;
+  for (let i = 1; i < len; i++) {
+    val = arr[i];
+    if (val < min) {
+      min = val;
+      idx.length = 0;
+      idx.push(i);
+    } else if (val === min) {
+      idx.push(i);
+    }
+  }
+  return idx;
 };
 
 const onlyUnique = (value: string, index: any, self: any) => {
-	return self.indexOf(value) === index;
-}
+  return self.indexOf(value) === index;
+};
 
 const onlyDamage = (value: string) => {
-	return value.indexOf("0 - 0%") === -1;
-}
+  return value.indexOf('0 - 0%') === -1;
+};
 
 const handleCalcs = (output: string, key: string, list: any) => {
-	output += '<th scope="row">';
-	output += key;
-	output += '</th>';
-	for (var k in list) {
-		list[k] = list[k].filter(onlyUnique);
-		list[k] = list[k].filter(onlyDamage);
-		output += '<td>';
+  output += '<th scope="row">';
+  output += key;
+  output += '</th>';
+  for (const k in list) {
+    list[k] = list[k].filter(onlyUnique);
+    list[k] = list[k].filter(onlyDamage);
+    output += '<td>';
 
-		output += '<ul class="list-group">';
+    output += '<ul class="list-group">';
 
-		for (var listKey in list[k]) {
-			var listVal = list[k][listKey];
-			var move = listVal.substr(0, listVal.lastIndexOf(':'));
-			var percentage = listVal.substr(listVal.lastIndexOf(':') + 2);
-			percentage = percentage.replace('(', '').replace(')', '');
-			output += '<li class="list-group-item"><span class="badge">';
-			output += percentage;
-			output += '</span>';
-			output += move;
-			output += '</li>';
-		}
+    for (const listKey in list[k]) {
+      const listVal = list[k][listKey];
+      const move = listVal.substr(0, listVal.lastIndexOf(':'));
+      let percentage = listVal.substr(listVal.lastIndexOf(':') + 2);
+      percentage = percentage.replace('(', '').replace(')', '');
+      output += '<li class="list-group-item"><span class="badge">';
+      output += percentage;
+      output += '</span>';
+      output += move;
+      output += '</li>';
+    }
 
-		output += '</ul>';
+    output += '</ul>';
 
-		output += '</td>';
-	}
-	return output;
+    output += '</td>';
+  }
+  return output;
 };
 
 // Exported functions
 
-const translateEvs = (pokeInfo : any) => {
-	if ("at" in pokeInfo.evs) pokeInfo.evs.atk = pokeInfo.evs.at;
-	if ("df" in pokeInfo.evs) pokeInfo.evs.def = pokeInfo.evs.df;
-	if ("sa" in pokeInfo.evs) pokeInfo.evs.spa = pokeInfo.evs.sa;
-	if ("sd" in pokeInfo.evs) pokeInfo.evs.spd = pokeInfo.evs.sd;
-	if ("sp" in pokeInfo.evs) pokeInfo.evs.spe = pokeInfo.evs.spe;
+const translateEvs = (pokeInfo: any) => {
+  if ('at' in pokeInfo.evs) pokeInfo.evs.atk = pokeInfo.evs.at;
+  if ('df' in pokeInfo.evs) pokeInfo.evs.def = pokeInfo.evs.df;
+  if ('sa' in pokeInfo.evs) pokeInfo.evs.spa = pokeInfo.evs.sa;
+  if ('sd' in pokeInfo.evs) pokeInfo.evs.spd = pokeInfo.evs.sd;
+  if ('sp' in pokeInfo.evs) pokeInfo.evs.spe = pokeInfo.evs.spe;
 };
 
-const getSearchParameters = () : any => {
-	var prmstr = window.location.search.substr(1);
-	return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
-} ;
+const getSearchParameters = (): any => {
+  const prmstr = window.location.search.substr(1);
+  return prmstr != null && prmstr != '' ? transformToAssocArray(prmstr) : {};
+};
 
 const transformToAssocArray = (prmstr: string) => {
-	let params = {} as any;
-	let prmarr = prmstr.split("&");
-	for (var i = 0; i < prmarr.length; i++) {
-		let tmparr = prmarr[i].split("=");
-		params[tmparr[0]] = tmparr[1];
-	}
-	return params;
+  const params = {} as any;
+  const prmarr = prmstr.split('&');
+  for (let i = 0; i < prmarr.length; i++) {
+    const tmparr = prmarr[i].split('=');
+    params[tmparr[0]] = tmparr[1];
+  }
+  return params;
 };
 
 const handleInput = (input: string) => {
-	var handledInput = input;
-	handledInput = handledInput.charAt(0).toUpperCase() + handledInput.slice(1);
-	if (handledInput.indexOf('-') !== -1) {
-		var charIndex = handledInput.lastIndexOf('-') + 1;
-		handledInput = handledInput.slice(0, charIndex) + handledInput.charAt(charIndex).toUpperCase() + handledInput.slice(charIndex + 1);
-	}
-	return handledInput;
+  let handledInput = input;
+  handledInput = handledInput.charAt(0).toUpperCase() + handledInput.slice(1);
+  if (handledInput.indexOf('-') !== -1) {
+    const charIndex = handledInput.lastIndexOf('-') + 1;
+    handledInput =
+      handledInput.slice(0, charIndex) +
+      handledInput.charAt(charIndex).toUpperCase() +
+      handledInput.slice(charIndex + 1);
+  }
+  return handledInput;
 };
 
 const getSetPokemon = (setdex: any, pokemon: string) => {
-	if (pokemon in setdex) {
-		return pokemon;
-	}
-	var setKeys = Object.keys(setdex);
-	for (var i = 0; i < setKeys.length; i++) {
-		if (setKeys[i].indexOf(pokemon) !== -1) {
-			return setKeys[i];
-		}
-	}
-	var chosenKey = argmin(setKeys.map((f) => getDifference(pokemon, f).length));
-	pokemon = setKeys[chosenKey[0]];
-	return pokemon;
-};
-					
-const isTierAvailable = (setCompilation: {}, mode: string) => {
-	for (var key in setCompilation) {
-		if (key.toLowerCase().indexOf(mode + " ") !== -1) {
-			return true;
-		}
-	}
-	return false;
+  if (pokemon in setdex) {
+    return pokemon;
+  }
+  const setKeys = Object.keys(setdex);
+  for (let i = 0; i < setKeys.length; i++) {
+    if (setKeys[i].indexOf(pokemon) !== -1) {
+      return setKeys[i];
+    }
+  }
+  const chosenKey = argmin(
+    setKeys.map((f) => getDifference(pokemon, f).length)
+  );
+  pokemon = setKeys[chosenKey[0]];
+  return pokemon;
 };
 
-declare var notation: any;
+const isTierAvailable = (setCompilation: any, mode: string) => {
+  for (const key in setCompilation) {
+    if (key.toLowerCase().indexOf(mode + ' ') !== -1) {
+      return true;
+    }
+  }
+  return false;
+};
+
+declare let notation: any;
 declare class Move {
-	constructor(gen: any, poke: any);
-};
-declare function calculate(generation: any, pokeOwn: any, pokeOpp: any, moveOwn: any, filler: any): any;
+  constructor(gen: any, poke: any);
+}
+declare function calculate(
+  generation: any,
+  pokeOwn: any,
+  pokeOpp: any,
+  moveOwn: any,
+  filler: any
+): any;
 
-const describeCalcs = (pokeOwn: any, gen: any, pokeOpp: any, generation: any, calcs: any, key: any, key2: any) : void => {
-	for (var i = 0; i < pokeOwn.moves.length; i++) {
-		var moveOwn = new Move(gen, pokeOwn.moves[i]);
-		for (var j = 0; j < pokeOpp.moves.length; j++) {
-			var calc = calculate(generation, pokeOwn, pokeOpp, moveOwn, undefined);
-			var desc = calc.fullDesc(notation, null);
-			var def = calc.rawDesc.moveName;
-			var range = desc.substr(desc.lastIndexOf('('));
-			range = range.substr(0, range.indexOf(')') + 1);
-			def = def + ': ' + range;
-			calcs[key][key2].push(def);
-		}
-	}
+const describeCalcs = (
+  pokeOwn: any,
+  gen: any,
+  pokeOpp: any,
+  generation: any,
+  calcs: any,
+  key: any,
+  key2: any
+): void => {
+  for (let i = 0; i < pokeOwn.moves.length; i++) {
+    const moveOwn = new Move(gen, pokeOwn.moves[i]);
+    for (let j = 0; j < pokeOpp.moves.length; j++) {
+      const calc = calculate(generation, pokeOwn, pokeOpp, moveOwn, undefined);
+      const desc = calc.fullDesc(notation, null);
+      let def = calc.rawDesc.moveName;
+      let range = desc.substr(desc.lastIndexOf('('));
+      range = range.substr(0, range.indexOf(')') + 1);
+      def = def + ': ' + range;
+      calcs[key][key2].push(def);
+    }
+  }
 };
 
-const handleOutput = (ownPokemon: string, oppPokemon: string, owncalcs: any, oppcalcs: any) : string => {
-	let output = '';
-	output += '<table class="table table-condensed">';
-	output += '<thead><th scope="col">' + ownPokemon + '</th>';
-	for (var key in oppcalcs) {
-		output += '<th scope="col">' + oppPokemon + ' - ' + key + '</th>';
-	}
-	output += '</thead>';
-	output += '<tbody>';
-	for (var key in owncalcs) {
-		output += '<tr>';
-		output = handleCalcs(output, key, owncalcs[key]);
-		output += '</tr>';
-	}
-	output += '</tbody></table>';
-	return output;
+const handleOutput = (
+  ownPokemon: string,
+  oppPokemon: string,
+  owncalcs: any,
+  oppcalcs: any
+): string => {
+  let output = '';
+  output += '<table class="table table-condensed">';
+  output += '<thead><th scope="col">' + ownPokemon + '</th>';
+  for (const key in oppcalcs) {
+    output += '<th scope="col">' + oppPokemon + ' - ' + key + '</th>';
+  }
+  output += '</thead>';
+  output += '<tbody>';
+  for (const key in owncalcs) {
+    output += '<tr>';
+    output = handleCalcs(output, key, owncalcs[key]);
+    output += '</tr>';
+  }
+  output += '</tbody></table>';
+  return output;
 };
 
 export {
-	translateEvs,
-	getSearchParameters,
-	handleInput,
-	getSetPokemon,
-	isTierAvailable,
-	describeCalcs,
-	handleOutput,
+  translateEvs,
+  getSearchParameters,
+  handleInput,
+  getSetPokemon,
+  isTierAvailable,
+  describeCalcs,
+  handleOutput,
 };
